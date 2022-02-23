9ecc29f82fd6bc760df716d05f622bb708529983
Wesed <dev.wfeduardo@gmail.com>
2022-02-23 17:22:52 -0300

    estilizando o background do formulario com Grid Layout

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from './../../UserContext';
+import styles from './Login.module.css'
 
 
 const Login = () => {
@@ -15,14 +16,16 @@ const Login = () => {
   corrigir com um estilo talvez */
   if(login === true) return <Navigate to="/conta"/>;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm/>} />
-        <Route path="criar" element={<LoginCreate/>} />
-        <Route path="perdeu" element={<LoginPasswordLost/>} />
-        <Route path="resetar" element={<LoginPasswordReset/>} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm/>} />
+          <Route path="criar" element={<LoginCreate/>} />
+          <Route path="perdeu" element={<LoginPasswordLost/>} />
+          <Route path="resetar" element={<LoginPasswordReset/>} />
+        </Routes>
+      </div>
+    </section>
   )
 }
 
