4ce9a86d56e031743831245f15bb9b675d5a2aad
Wesed <dev.wfeduardo@gmail.com>
2022-02-26 14:50:23 -0300

    Criacao do menu hamburguer (menu mobile)

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,19 +1,37 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 import {ReactComponent as MinhasFotos} from '../../Assets/feed.svg';
 import {ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg';
 import {ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg';
 import {ReactComponent as Sair} from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import UseMedia from './../../Hooks/UseMedia';
 
 const UserHeaderNav = () => {
 
   const {userLogout} = React.useContext(UserContext);
-  const [mobile, setMobile] = React.useState(null);
+  const mobile = UseMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  // sempre que o pathname mudar (clicar em algum menu), fecha o menu
+  const {pathname} = useLocation();
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
   return (
-    <nav className={styles.nav}>
+  <>    
+    {mobile && (
+    <button 
+    aria-label="Menu"
+    // so vai ter a class mobileButtonActive se for true
+    className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+    // efeito toggle
+    onClick={() => setMobileMenu(!mobileMenu)}
+    ></button>)}
+
+    <nav className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive} `}>
       <NavLink to="/conta" end>
         <MinhasFotos/>
         {mobile && 'Minhas fotos'}
@@ -34,6 +52,7 @@ const UserHeaderNav = () => {
         {mobile && 'Sair'}
       </button>
     </nav>
+  </>
   )
 }
 
