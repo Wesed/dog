cfa63f051f435beeb1118c90af9043b38430e84d
Wesed <dev.wfeduardo@gmail.com>
2022-03-08 17:23:52 -0300

    Automatizando o carregamento de novas publicacoes ao dar scroll no mouse (scroll infinito)

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -4,13 +4,15 @@ import UserHeader from './UserHeader';
 import Feed from './../Feed/Feed';
 import UserPhotoPost from './../Feed/UserPhotoPost';
 import UserStats from './../Feed/UserStats';
+import { UserContext } from './../../UserContext';
 
 const User = () => {
+  const {data} = React.useContext(UserContext);
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />}/>
+        <Route path="/" element={<Feed user={data.id} />}/>
         <Route path="/estatisticas" element={<UserStats />}/>
         <Route path="/postar" element={<UserPhotoPost />}/>
       </Routes>
