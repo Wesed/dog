a4a5c82d96088d9d31e5a3e7941f410781562187
Wesed <dev.wfeduardo@gmail.com>
2022-03-11 11:42:05 -0300

    Criando a pagina que exibe as estatisticas das fotos do usuario

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import Head from './../Helper/Head';
+import React from "react";
+import Head from "./../Helper/Head";
+import useFetch from "./../../Hooks/useFetch";
+import { GET_STATS } from "./../../api";
+import Loading from "./../Helper/Loading";
+import Error from "./../Helper/Error";
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem("token");
+      const { url, options } = GET_STATS(token);
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
       </div>
-  )
-}
+    );
+  else return null;
+};
 
 export default UserStats;
