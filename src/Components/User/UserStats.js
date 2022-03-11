50b34114e91fcdeb902da275cf59c5ebbdf77751
Wesed <dev.wfeduardo@gmail.com>
2022-03-11 15:55:28 -0300

    Adicionando o Lazy & Suspense antes no component que importa o Victory

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -4,8 +4,8 @@ import useFetch from "./../../Hooks/useFetch";
 import { GET_STATS } from "./../../api";
 import Loading from "./../Helper/Loading";
 import Error from "./../Helper/Error";
-import UserStatsGraphs from './UserStatsGraphs';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+const UserStatsGraphs = React.lazy(() => import("./UserStatsGraphs"));
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -19,23 +19,25 @@ const UserStats = () => {
     getData();
   }, [request]);
 
-  if (loading) return <Loading />;
   if (error) return <Error error={error} />;
 
-  if(data && data.length > 0)
-      return (
-        <div>
-          <Head title="Estatísticas" />
-          <UserStatsGraphs data={data} />
-        </div>
-      );
-    
-  else return (
-    <div className="UserStatsLink">
-      <p> Não há nenhuma postagem desse usuário.</p>
-      <Link style={{opacity: .8}} to={'/conta/postar'}> Postar uma nova foto </Link>
-    </div>
-  )
+  if (data && data.length > 0)
+    return (
+      // so importa o Victory qd clicar em estatisticas
+      <React.Suspense fallback={loading && <Loading />}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  else
+    return (
+      <div className="UserStatsLink">
+        <p> Não há nenhuma postagem desse usuário.</p>
+        <Link style={{ opacity: 0.8 }} to={"/conta/postar"}>
+          Postar uma nova foto
+        </Link>
+      </div>
+    );
 };
 
 export default UserStats;
