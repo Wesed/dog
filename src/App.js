12051a3f4d8e005ed3361d6b203581fee17e3885
Wesed <dev.wfeduardo@gmail.com>
2022-03-11 10:29:59 -0300

    Modificando o ProtectedRoute.js, afim de evitar o bug que aparece a tela de login ao atualizar a pagina de usuario

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import {UserStorage} from './UserContext';
-import ProtectedRoute from './Components/Helper/ProtectedRoute';
-import User from './Components/User/User';
-import Photo from './Components/Photo/Photo';
-import UserProfile from './Components/User/UserProfile';
-import NotFound from './Components/NotFound';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import User from "./Components/User/User";
+import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <>
-    <BrowserRouter>
-      <UserStorage>
-        <Header />
-        <Routes>
-          {/* path faz referencia ao login.js (caminho) */
-          /* ja o element faz referencia ao conteudo retornado pelo login.js */}
-          <Route path="/" element={<Home />} />
-          <Route path="login/*" element={<Login />} />
-          <Route path="conta/*" element={
-          <ProtectedRoute>
-            <User/>
-          </ProtectedRoute>} />
-          <Route path="foto/:id" element={<Photo />} />
-          <Route path="perfil/:user" element={<UserProfile />} />
-          <Route path="*" element={<NotFound />}/>
-        </Routes>
-        <Footer />
-      </UserStorage>
-    </BrowserRouter>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <main className="AppBody">
+            <Routes>
+              {/* path faz referencia ao login.js (caminho) */
+              /* ja o element faz referencia ao conteudo retornado pelo login.js */}
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 }
 
